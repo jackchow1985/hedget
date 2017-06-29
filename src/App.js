122b683850fa9587d4b285d72b1d7e636aa87cac
Jack <jackchow1985@gmail.com>
2017-06-29 18:51:48 +0800

    add code editor plugin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import CodeMirror from 'react-codemirror';
+import Css from 'codemirror/lib/codemirror.css';
+require('codemirror/mode/python/python');
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.updateCode = this.updateCode.bind(this);
+    this.state = {code: "123"};
   }
+
+  updateCode() {
+
+  }
+	render() {
+		var options = {
+			lineNumbers: true,
+      mode: 'python'
+		};
+		return <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+	}
 }
+
 
 export default App;
