832d3e694c22d1c43c1f86e1addc379aac302d1f
Jack <jackchow1985@gmail.com>
2017-08-11 15:16:13 +0800

    edit readme

--- src/strategies/list.js
+++ src/strategies/list.js
@@ -33,52 +33,53 @@ class StrategiesList extends Component {
   render() {
     return (
       <div className="panel">
-                <div className="panel-header text-center">
-                  <div className="avatar avatar-lg">
-                    <img src="https://picturepan2.github.io/spectre/img/avatar-2.png" alt="avatar"/>
-                  </div>
-                  <div className="panel-title mt-10">{this.props.username}</div>
-                  <div className="panel-subtitle">策略数：{this.state.list.length}</div>
+        <div className="panel-header text-center">
+          <div className="avatar avatar-lg">
+            <img src="https://picturepan2.github.io/spectre/img/avatar-2.png" alt="avatar"/>
+          </div>
+          <div className="panel-title mt-10">{this.props.username}</div>
+          <div className="panel-subtitle">策略数：{this.state.list.length}</div>
+        </div>
+        <nav className="panel-nav">
+          <ul className="tab tab-block">
+            <li className="tab-item active">
+              <a href="#panels">
+                默认
+              </a>
+            </li>
+            <li className="tab-item">
+              <a href="#panels">
+                实盘
+              </a>
+            </li>
+            <li className="tab-item">
+              <a href="#panels">
+                Paper Test
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="panel-body">
+
+          {this.state.list && !this.state.list.err && this.state.list.map(i => {
+            return (
+              <div className="tile tile-centered strategy-item" onClick={() => this.handleSelect(i)} key={i._id}>
+                <div className="tile-content">
+                  <div className="tile-title">{i.title}</div>
+                  <div className="tile-subtitle">{moment(i.updateTime).fromNow()}</div>
                 </div>
-                <nav className="panel-nav">
-                  <ul className="tab tab-block">
-                    <li className="tab-item active">
-                      <a href="#panels">
-                        默认
-                      </a>
-                    </li>
-                    <li className="tab-item">
-                      <a href="#panels">
-                        实盘
-                      </a>
-                    </li>
-                    <li className="tab-item">
-                      <a href="#panels">
-                        Paper Test
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-                <div className="panel-body">
-
-
-                  {this.state.list && !this.state.list.err && this.state.list.map(i => {
-                    return (
-                      <div className="tile tile-centered strategy-item" onClick={() => this.handleSelect(i)} key={i._id}>
-                        <div className="tile-content">
-                          <div className="tile-title">{i.title}</div>
-                          <div className="tile-subtitle">{moment(i.updateTime).fromNow()}</div>
-                        </div>
-                        <div className="tile-action">
-                          <button className="btn btn-link btn-action btn-lg"><i className="icon icon-edit"></i></button>
-                        </div>
-                      </div>
-
-                    )
-                  })}
+                <div className="tile-action">
+                  <button className="btn btn-link btn-action btn-lg">
+                    <i className="icon icon-edit"></i>
+                  </button>
                 </div>
-
               </div>
+
+            )
+          })}
+        </div>
+
+      </div>
 
     )
   }
